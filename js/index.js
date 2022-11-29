8062d95fd63b703b01b9f08dfaf3ad61e0e63e78
Lars010 <102806375+Lars010@users.noreply.github.com>
2022-11-29 11:26:57 -0500

    Update

--- js/index.js
+++ js/index.js
@@ -1,19 +1,31 @@
-import { getPokemon, getSpecies } from "./api.js";
-
+import { setPokemon } from "./pokedex.js"
 
 const $form = document.querySelector('#form')
-const $image = document.querySelector('#image')
-const $description = document.querySelector('#description')
+const $next = document.querySelector('#next-pokemon')
+const $prev = document.querySelector('#prev-pokemon')
+const $pokedex = document.querySelector('#pokedex')
 
 
-$form.addEventListener('submit', async (event) => {
+
+$form.addEventListener('submit', handleSubmit)
+$next.addEventListener('click', handleNextPokemon)
+$prev.addEventListener('click', handlePrevPokemon)
+
+let activePokemon = null
+async function handleSubmit(event) {
     event.preventDefault()
+    $pokedex.classList.add('is-open')
     const form = new FormData($form)
     const id = form.get('id')
-    const pokemon = await getPokemon(id)
-    const species = await getSpecies(id)
-    const description = species.flavor_text_entries.find((flavor) => flavor.language.name === 'en')
-    $image.src = pokemon.sprites.front_default
-    $description.textContent = description.flavor_text
-    // console.log(description)
-})
+    activePokemon = await setPokemon(id)
+}
+
+async function handleNextPokemon() {
+    const id = (activePokemon === null || activePokemon.id === 898) ? 1 : activePokemon.id + 1
+    activePokemon = await setPokemon(id)
+}
+
+async function handlePrevPokemon() {
+    const id = (activePokemon === null || activePokemon.id === 1) ? 898 : activePokemon.id - 1
+    activePokemon = await setPokemon(id)
+}
