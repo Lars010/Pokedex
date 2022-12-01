41684b3f0d21171b903e97680cc5e5d0bf760402
Lars010 <102806375+Lars010@users.noreply.github.com>
2022-12-01 12:24:14 -0500

    Update + 1

--- js/api.js
+++ js/api.js
@@ -3,14 +3,23 @@ const BASE_API = 'https://pokeapi.co/api/v2/'
 
 export async function getPokemon(id){
     const response = await fetch(`${BASE_API}pokemon/${id}/`)
-    const data = await response.json()
+    if(response.status === 200) return await response.json();
+    // const data = await response.json()
     //TAREA :validar Errores de tarea
-    return data
-}
+    throw await response.text();
+    // return data
+    
+} 
+    
+
+   
+
 
 export async function getSpecies(id) {
     const response = await fetch(`${BASE_API}pokemon-species/${id}/`)
-    const data = await response.json()
+    if(response.status === 200) return await response.json();
+    // const data = await response.json()
     //TAREA :validar Errores de tarea
-    return data
+    // return data
+    throw await response.text();
 }
--- js/pokedex.js
+++ js/pokedex.js
@@ -1,7 +1,8 @@
 import { getPokemon, getSpecies } from "./api.js";
+import { createChart } from "./charts.js"
 
 const $image = document.querySelector('#image')
-function setImage(image) {
+export function setImage(image) {
     $image.src = image
 }
 
@@ -16,27 +17,61 @@ function loader(isLoading = false) {
     const img = isLoading ? 'url(./images/loading.gif)' : ''
     $screen.style.backgroundImage = img
 }
+const $light = document.querySelector('#light')
+function speech(text) {
+    const utterance = new SpeechSynthesisUtterance(text)
+    utterance.lang = 'en'
+    speechSynthesis.speak(utterance)
+    $light.classList.add('is-animated')
+    utterance.addEventListener ('end', () => {
+        $light.classList.remove('is-animated')
+    })
+}
 
 
 export async function findPokemon(id) {
+    
+    try {
+
     const pokemon = await getPokemon(id)
     const species = await getSpecies(id)
     const description = species.flavor_text_entries.find((flavor) => flavor.language.name === 'en')
-    return{
-        sprites: pokemon.sprites.front_default,
-        description: description.flavor_text,
-        id: pokemon.id
+    const sprites = [pokemon.sprites.front_default]
+    const stats = pokemon.stats.map(item => item.base_stat)
+    for (const item in pokemon.sprites) {
+        if(item !== 'front_default' && item !== 'other' && item !== 'versions' && pokemon.sprites[item]){
+            sprites.push(pokemon.sprites[item])
+        }
     }
-    
+    return {
+        sprites,
+        description: description.flavor_text,
+        id: pokemon.id,
+        name: pokemon.name,
+        stats,
+    };
+} catch (e) {
+    //solucionar por que no funciona el catch ahora
+    return {
+        description: 'Pokemon not found'
+    }
 }
+} 
 
+let activeChart = null
 export async function setPokemon(id) {
     //prender loader
     loader(true)
+    document.getElementById('pokemonNumber').value = id
     const pokemon = await findPokemon(id)
     //apagar loader
     loader(false)
-    setImage(pokemon.sprites)
+    setImage(pokemon.sprites[0])
     setDescription(pokemon.description)
+    speech(`${pokemon.name}. ${pokemon.description}`)
+    if (activeChart instanceof Chart) {
+        activeChart.destroy()
+    }
+    activeChart = createChart(pokemon.stats)
     return pokemon
 }
